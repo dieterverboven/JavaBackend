96f42e2b473599c73f704974c23395b15f1c128b
Lorenz <lorenzcleymans@hotmail.com>
2018-11-19 16:53:55 +0100

    lorenz proberen te fixen

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ var bodyParser = require('body-parser');
 const beloningRoutes = require('./api/routes/Beloningen');
 const gebruikerRoutes = require('./api/routes/Gebruikers');
 
+mongoose.connect('mongodb://admin:admin@reward-system-shard-00-00-guwav.mongodb.net:27017,reward-system-shard-00-01-guwav.mongodb.net:27017,reward-system-shard-00-02-guwav.mongodb.net:27017/test?ssl=true&replicaSet=reward-system-shard-0&authSource=admin&retryWrites=true');
+
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use('/Beloningen', beloningRoutes);
-app.use('/Gebruikers', gebruikerRoutes);
+//app.use('/Gebruikers', gebruikerRoutes);
 
-mongoose.connect('mongodb://admin:admin@reward-system-shard-00-00-guwav.mongodb.net:27017,reward-system-shard-00-01-guwav.mongodb.net:27017,reward-system-shard-00-02-guwav.mongodb.net:27017/test?ssl=true&replicaSet=reward-system-shard-0&authSource=admin&retryWrites=true');
 
 var db = mongoose.connection;
 
