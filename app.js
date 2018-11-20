4e501eb4cdde3076fa5cd8b50e87173e68fc8ee4
Lorenz <lorenzcleymans@hotmail.com>
2018-11-20 09:42:53 +0100

    Lorenz aanpassingen

--- app.js
+++ app.js
@@ -7,13 +7,12 @@ var bodyParser = require('body-parser');
 const beloningRoutes = require('./api/routes/Beloningen');
 const gebruikerRoutes = require('./api/routes/Gebruikers');
 
-mongoose.connect('mongodb://admin:admin@reward-system-shard-00-00-guwav.mongodb.net:27017,reward-system-shard-00-01-guwav.mongodb.net:27017,reward-system-shard-00-02-guwav.mongodb.net:27017/test?ssl=true&replicaSet=reward-system-shard-0&authSource=admin&retryWrites=true');
-
+mongoose.connect('mongodb://localhost:27017/test');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use('/Beloningen', beloningRoutes);
-//app.use('/Gebruikers', gebruikerRoutes);
+app.use('/Gebruikers', gebruikerRoutes);
 
 
 var db = mongoose.connection;
@@ -22,7 +21,7 @@ app.get('/', (req, res)=> {
    res.send('Hello World');
 
 });
-
-app.listen(3000, ()=>{
-    console.log("Server is running");
+let port = 3000;
+app.listen(port, ()=>{
+    console.log("Server is running on port: "+ port);
 });
