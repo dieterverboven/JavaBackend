4c848f1e4b09a6a73a4c74d50f303dfeafbb41c2
dieterverboven <dieter.verboven@outlook.com>
2018-11-20 09:01:59 +0100

    routes naar opdrachten/voltooideOpdrachten, HTTP requests voor Opdrachten in orde

---
+++ api/routes/Opdrachten.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+const Opdracht = require('../models/Opdracht');
+
+//get all opdrachten
+router.get('/', (req, res, next)=> {
+    Opdracht.find()
+    .exec()
+    .then(docs => {
+        console.log(docs);
+        res.status(200).json(docs);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        })
+    });
+});
+
+// get opdracht by id
+router.get('/:opdrachtId', (req, res, next)=> {
+    const id = req.params.opdrachtId;
+    Opdracht.findById(id)
+    .exec()
+    .then(doc => {
+        console.log(doc);
+        console.log('hehe');
+        res.status(200).json(doc);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        })
+    });
+});
+
+//nieuwe opdracht aanmaken
+router.post('/', (req, res, next)=> {
+    const opdracht = new Opdracht({
+        _id: new mongoose.Types.ObjectId,
+        uitleg: req.body.uitleg,
+        punten: req.body.punten
+    });
+    opdracht
+    .save()
+    .then(result => {
+        console.log(result);
+    })
+    .catch(err => console.log(err));
+    res.status(201).json({
+        message: 'Bezig',
+        aangemaakteOpdracht: opdracht
+    })
+});
+
+// opdracht verwijderen
+router.delete('/:opdrachtId', (req, res, next)=> {
+    const id = req.params.opdrachtId;
+    Opdracht.remove({_id: id})
+    .exec()
+    .then(result => {
+        res.status(200).json(result);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        })
+    });
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -2,10 +2,11 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
 
 const beloningRoutes = require('./api/routes/Beloningen');
 const gebruikerRoutes = require('./api/routes/Gebruikers');
+const opdrachtRoutes = require('./api/routes/Opdrachten');
+const voltooideOpdrachtRoutes = require('./api/routes/voltooideOpdrachten');
 
 mongoose.connect("mongodb://admin:admin@reward-system-shard-00-00-guwav.mongodb.net:27017,reward-system-shard-00-01-guwav.mongodb.net:27017,reward-system-shard-00-02-guwav.mongodb.net:27017/test?ssl=true&replicaSet=reward-system-shard-0&authSource=admin&retryWrites=true");
 
@@ -13,6 +14,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use('/Beloningen', beloningRoutes);
 app.use('/Gebruikers', gebruikerRoutes);
+app.use('/Opdrachten', opdrachtRoutes);
+app.use('/voltooideOpdrachten', voltooideOpdrachtRoutes);
 
 
 var db = mongoose.connection;
